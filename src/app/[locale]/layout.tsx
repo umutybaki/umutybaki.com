f6106705dab6db1583072f2003ee3b5978b20d90
Umut Baki <umutybaki@outlook.com>
2026-05-21 16:12:54 +0300

    add google adsense

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -34,7 +34,8 @@ export default async function LocaleLayout({ children, params }: Props) {
     <html lang={locale} data-theme={theme} suppressHydrationWarning>
       <head>
         <script src="/theme-init.js" />
-        {/* Google Analytics */}
+        {/* Google AdSense */}
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6405541080280025" crossorigin="anonymous"></script>        {/* Google Analytics */}
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-ZZ4R2RV2NP" />
         <script
           dangerouslySetInnerHTML={{
